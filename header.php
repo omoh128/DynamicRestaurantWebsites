d0ed557761b965c92ea1cdd2b194dcaa4db97bd5
ommoh <omoh128@gmail.com>
2017-10-26 11:11:15 +0200

    first commit

---
+++ header.php
@@ -0,0 +1,28 @@
+<?php
+	$companyName = "Restaurant";
+	include('includes/MenuItem.php');
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title><?php echo TITLE; ?></title>
+	<link href="/Css/styles.css" rel="stylesheet">
+</head>
+
+<body id="final-example">
+	
+	<div class="wrapper">
+	
+		<div id="banner">
+			<a href="/" title="Return to Home">
+				<img src="img/banner.png" alt="Restaurant">
+			</a>	
+		</div><!-- banner -->
+		
+		<div id="nav">
+			<?php include('includes/nav.php'); ?>
+		</div><!-- nav -->
+		
+		<div class="content">
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+	define("TITLE", "Home | Restaurant");
+	
+	include('includes/header.php');
+?>
+
+	<div id="philosophy">
+		<hr>
+		
+		<h1>WelCome To Our Restaurant</h1>
+		<p>Here at Di Restaurant, we know that good food isn't just about how expensive the dish is. We're not pompous, we're proud. We're proud of our work, our quality, our environment, and our love for food and family.</p>
+		<p>Oh, and you can call us Frankie's. You're family here.</p>
+		
+		<hr>
+	</div><!-- philosophy -->
+			
+<?php include('includes/footer.php'); ?>
